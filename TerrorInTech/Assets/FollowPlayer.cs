a0973a4c8c9dcbf185c1987a73b5ff6179f05ebe
OwenJanssen <67287024+OwenJanssen@users.noreply.github.com>
2022-02-06 16:36:45 -0600

    Create FollowPlayer.cs

---
+++ TerrorInTech/Assets/FollowPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 camera_offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera_offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + camera_offset;
+    }
+}
