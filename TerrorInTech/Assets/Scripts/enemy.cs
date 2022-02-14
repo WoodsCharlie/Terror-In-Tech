3b119745fcadf7129a14801368c92a2ced75e7dd
WoodsCharlie <62679184+WoodsCharlie@users.noreply.github.com>
2022-02-14 13:56:35 -0600

    Geese Rotate and better aiming
    The geese spin to look at you now and bullets shoot straight out of the gun

--- TerrorInTech/Assets/Scripts/enemy.cs
+++ TerrorInTech/Assets/Scripts/enemy.cs
@@ -16,8 +16,19 @@ public class enemy : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
+        TurnToPlayer();
         transform.position = Vector3.MoveTowards(transform.position, Player.position, speed);
+    }
+
+    void TurnToPlayer()
+    {
+        Vector3 player_pos = Player.position;
+        player_pos.z = 0f;
+        player_pos.x = player_pos.x - transform.position.x;
+        player_pos.y = player_pos.y - transform.position.y;
+
+        float angle = Mathf.Atan2(player_pos.y, player_pos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
     void OnCollisionEnter2D(Collision2D collision)
