cd71c7dbb9373fe93f34c778d4f88856468a4ec2
OwenJanssen <67287024+OwenJanssen@users.noreply.github.com>
2022-02-27 15:43:14 -0600

    Update Shooter.cs

--- TerrorInTech/Assets/Scripts/Shooter.cs
+++ TerrorInTech/Assets/Scripts/Shooter.cs
@@ -39,7 +39,7 @@ public class Shooter : MonoBehaviour
                 
             if (PlayerPrefs.GetInt("ammo_selection") == 2 & PlayerPrefs.GetInt("yellow") > 0)
             {
-                PlayerPrefs.SetInt("orange", PlayerPrefs.GetInt("yellow") - 1);
+                PlayerPrefs.SetInt("yellow", PlayerPrefs.GetInt("yellow") - 1);
                 GameObject bullet = Instantiate(shotPrefab, firePoint.position, firePoint.rotation);
                 Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
                 body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
@@ -47,7 +47,7 @@ public class Shooter : MonoBehaviour
 
             if (PlayerPrefs.GetInt("ammo_selection") == 3 & PlayerPrefs.GetInt("green") > 0)
             {
-                PlayerPrefs.SetInt("orange", PlayerPrefs.GetInt("green") - 1);
+                PlayerPrefs.SetInt("green", PlayerPrefs.GetInt("green") - 1);
                 GameObject bullet = Instantiate(shotPrefab, firePoint.position, firePoint.rotation);
                 Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
                 body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
@@ -55,7 +55,7 @@ public class Shooter : MonoBehaviour
                 
             if (PlayerPrefs.GetInt("ammo_selection") == 4 & PlayerPrefs.GetInt("blue") > 0)
             {
-                PlayerPrefs.SetInt("orange", PlayerPrefs.GetInt("blue") - 1);
+                PlayerPrefs.SetInt("blue", PlayerPrefs.GetInt("blue") - 1);
                 GameObject bullet = Instantiate(shotPrefab, firePoint.position, firePoint.rotation);
                 Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
                 body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
@@ -63,7 +63,7 @@ public class Shooter : MonoBehaviour
             
             if (PlayerPrefs.GetInt("ammo_selection") == 5 & PlayerPrefs.GetInt("purple") > 0)
             {
-                PlayerPrefs.SetInt("orange", PlayerPrefs.GetInt("purple") - 1);
+                PlayerPrefs.SetInt("purple", PlayerPrefs.GetInt("purple") - 1);
                 GameObject bullet = Instantiate(shotPrefab, firePoint.position, firePoint.rotation);
                 Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
                 body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
