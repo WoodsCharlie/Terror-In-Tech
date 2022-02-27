788235e89b1c3c74783d208fd28015e46bee0f31
OwenJanssen <67287024+OwenJanssen@users.noreply.github.com>
2022-02-27 16:25:13 -0600

    Death Screen Added

--- TerrorInTech/Assets/Scripts/StartGame.cs
+++ TerrorInTech/Assets/Scripts/StartGame.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour
 {
+    public Text survivalText;
     public void startGame()
     {
         PlayerPrefs.DeleteAll();
         SceneManager.LoadScene("SampleScene");
+    }
+
+    private void FixedUpdate()
+    {
+        survivalText.text = "you and ian\nsurvived " + PlayerPrefs.GetInt("wave count").ToString() + " waves!";
     }
 }
