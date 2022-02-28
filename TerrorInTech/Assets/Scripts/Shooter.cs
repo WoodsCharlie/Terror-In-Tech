1fc87e195592d4a39eb50b0e43892d2eb7a1e237
WoodsCharlie <62679184+WoodsCharlie@users.noreply.github.com>
2022-02-28 02:24:06 -0600

    Ammo swap, hold to fire, reload
    Swaps back to red ammo when out of bullets, hold to fire with cooldown between shots, player has to reload every 12 shots

--- TerrorInTech/Assets/Scripts/Shooter.cs
+++ TerrorInTech/Assets/Scripts/Shooter.cs
@@ -15,18 +15,23 @@ public class Shooter : MonoBehaviour
     public GameObject purpleShotPrefab;
     public Transform Player;
 
-    public float shotForce = 5f; //aka speed (displacement?)
+    public float shotForce = 1000f; //aka speed (displacement?)
     public float shotDamage = 5f; 
+    public int shotsPerReload = 12;
+    public int shotsLeft;
+    public float reloadSpeed = 1f;
+    public float shotCooldownTotal = 0.025f;
+    public float shotCooldown = 0.025f;
 
     void Start()
     {
-        
+        shotsLeft = 12;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") & (shotCooldown >= shotCooldownTotal))
         {
             if (PlayerPrefs.GetInt("ammo_selection") == 0)
             {
@@ -35,62 +40,108 @@ public class Shooter : MonoBehaviour
                 body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
             }
                 
-            if (PlayerPrefs.GetInt("ammo_selection") == 1 & PlayerPrefs.GetInt("orange") > 0)
+            if (PlayerPrefs.GetInt("ammo_selection") == 1)
             {
-                PlayerPrefs.SetInt("orange", PlayerPrefs.GetInt("orange") - 1);
-                GameObject bullet = Instantiate(orangeShotPrefab, firePoint.position, firePoint.rotation);
-                Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
-                body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
+                if (PlayerPrefs.GetInt("orange") > 0)
+                {
+                    PlayerPrefs.SetInt("orange", PlayerPrefs.GetInt("orange") - 1);
+                    GameObject bullet = Instantiate(orangeShotPrefab, firePoint.position, firePoint.rotation);
+                    Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
+                    body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
+                }
+                else{
+                    PlayerPrefs.SetInt("ammo_selection", 0);
+                }
             }
                 
-            if (PlayerPrefs.GetInt("ammo_selection") == 2 & PlayerPrefs.GetInt("yellow") > 0)
+            if (PlayerPrefs.GetInt("ammo_selection") == 2)
             {
-                PlayerPrefs.SetInt("yellow", PlayerPrefs.GetInt("yellow") - 1);
-                for (int i = 0; i < 3; i++)
+                if (PlayerPrefs.GetInt("yellow") > 0)
                 {
-                    Vector3 temp = firePoint.up;
-                    float rotation = Player.rotation.z;
-                    if ((rotation >= 0.45 && rotation <= 0.90)){
-                        temp.y += (i - 1) * 0.5f;
-                    }
-                    else if (rotation <= -0.45 && rotation >= -0.90)
+                    PlayerPrefs.SetInt("yellow", PlayerPrefs.GetInt("yellow") - 1);
+                    for (int i = 0; i < 3; i++)
                     {
-                        temp.y += (i - 1) * 0.5f;
+                        Vector3 temp = firePoint.up;
+                        float rotation = Player.rotation.z;
+                        if ((rotation >= 0.45 && rotation <= 0.90)){
+                            temp.y += (i - 1) * 0.5f;
+                        }
+                        else if (rotation <= -0.45 && rotation >= -0.90)
+                        {
+                            temp.y += (i - 1) * 0.5f;
+                        }
+                        else
+                        {
+                            temp.x += (i - 1) * 0.5f;
+                        }
+                        GameObject bullet = Instantiate(greenShotPrefab, firePoint.position, firePoint.rotation);
+                        Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
+                        body.AddForce(temp * (float)shotForce/3, ForceMode2D.Impulse);
                     }
-                    else
-                    {
-                        temp.x += (i - 1) * 0.5f;
-                    }
-                    GameObject bullet = Instantiate(greenShotPrefab, firePoint.position, firePoint.rotation);
-                    Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
-                    body.AddForce(temp * (float)shotForce/3, ForceMode2D.Impulse);
+                }
+                else{
+                    PlayerPrefs.SetInt("ammo_selection", 0);
                 }
             }
 
-            if (PlayerPrefs.GetInt("ammo_selection") == 3 & PlayerPrefs.GetInt("green") > 0)
+            if (PlayerPrefs.GetInt("ammo_selection") == 3)
             {
-                PlayerPrefs.SetInt("green", PlayerPrefs.GetInt("green") - 1);
-                GameObject bullet = Instantiate(yellowShotPrefab, firePoint.position, firePoint.rotation);
-                Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
-                body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
+                if (PlayerPrefs.GetInt("green") > 0)
+                {
+                    PlayerPrefs.SetInt("green", PlayerPrefs.GetInt("green") - 1);
+                    GameObject bullet = Instantiate(yellowShotPrefab, firePoint.position, firePoint.rotation);
+                    Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
+                    body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
+                }
             }
                 
-            if (PlayerPrefs.GetInt("ammo_selection") == 4 & PlayerPrefs.GetInt("blue") > 0)
+            if (PlayerPrefs.GetInt("ammo_selection") == 4)
             {
-                PlayerPrefs.SetInt("blue", PlayerPrefs.GetInt("blue") - 1);
-                GameObject bullet = Instantiate(blueShotPrefab, firePoint.position, firePoint.rotation);
-                Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
-                body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
-            }   
-            
-            if (PlayerPrefs.GetInt("ammo_selection") == 5 & PlayerPrefs.GetInt("purple") > 0)
-            {
-                PlayerPrefs.SetInt("purple", PlayerPrefs.GetInt("purple") - 1);
-                GameObject bullet = Instantiate(purpleShotPrefab, firePoint.position, firePoint.rotation);
-                Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
-                body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
+                if (PlayerPrefs.GetInt("blue") > 0)
+                {
+                    PlayerPrefs.SetInt("blue", PlayerPrefs.GetInt("blue") - 1);
+                    GameObject bullet = Instantiate(blueShotPrefab, firePoint.position, firePoint.rotation);
+                    Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
+                    body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
+                }   
+                else{
+                    PlayerPrefs.SetInt("ammo_selection", 0);
+                }
             }
 
-        }   
+            if (PlayerPrefs.GetInt("ammo_selection") == 5)
+            {
+                if (PlayerPrefs.GetInt("purple") > 0)
+                {
+                    PlayerPrefs.SetInt("purple", PlayerPrefs.GetInt("purple") - 1);
+                    GameObject bullet = Instantiate(purpleShotPrefab, firePoint.position, firePoint.rotation);
+                    Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
+                    body.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
+                }
+                else{
+                    PlayerPrefs.SetInt("ammo_selection", 0);
+                }
+            }
+
+        shotCooldown = 0f;
+        shotsLeft = shotsLeft - 1;
+        }
+
+    if (Input.GetKeyDown("r") & (shotsLeft < shotsPerReload))
+    {
+        shotsLeft = shotsPerReload;
+        shotCooldown = shotCooldown - 1f;
+    }
+    
+    if (shotsLeft <= 0)
+    {
+        shotsLeft = shotsPerReload;
+        shotCooldown = shotCooldown - 1.25f;
+    }
+
+    if (shotCooldown < shotCooldownTotal){
+        shotCooldown += Time.deltaTime;
+    } 
+
     }
 }
